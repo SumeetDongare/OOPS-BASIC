0b98d8a626b37c27812782baff136b5f8e00c9ac
Sumeet Dongare <Sumeet.dongare19@gmail.com>
2026-08-23 16:34:23 +0530

    Add files via upload

---
+++ Scenari/Unit 1/scenario1.py
@@ -0,0 +1,44 @@
+class Mobile:
+    def __init__(self, brand, model, price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+    def get_category(self):
+        if self.price >= 50000:
+            return "Premium"
+        elif self.price >= 20000:
+            return "Mid-range"
+        else:
+            return "Budget"
+
+
+class Store:
+    def __init__(self):
+        self.mobiles = []
+
+    def add_mobile(self, mobile):
+        self.mobiles.append(mobile)
+
+    def display_mobiles(self):
+        print("\n--- Mobile Store ---")
+
+        for mobile in self.mobiles:
+            print("Brand   :", mobile.brand)
+            print("Model   :", mobile.model)
+            print("Price   :", mobile.price)
+            print("Category:", mobile.get_category())
+            print("--------------------")
+
+
+# Creating store
+store = Store()
+
+# Adding mobiles
+store.add_mobile(Mobile("Apple", "iPhone 15", 70000))
+store.add_mobile(Mobile("Samsung", "Galaxy A55", 35000))
+store.add_mobile(Mobile("Redmi", "Note 13", 15000))
+store.add_mobile(Mobile("OnePlus", "12R", 45000))
+
+# Displaying all mobiles
+store.display_mobiles()
